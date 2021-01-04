ad65ca278b58ac6f064bff1786af18da0300f8fc
Rakesh M S <msrakesh5991@gmail.com>
2021-01-04 11:55:23 +0530

    numworker set toone

--- test.py
+++ test.py
@@ -63,11 +63,11 @@ def main():
 	train_loader = torch.utils.data.DataLoader(training_data,
 											   batch_size=opt.batch_size,
 											   shuffle=True,
-											   num_workers=0)
+											   num_workers=1)
 	val_loader = torch.utils.data.DataLoader(validation_data,
 											 batch_size=opt.batch_size,
 											 shuffle=True,
-											 num_workers=0)
+											 num_workers=1)
 	print(f'Number of training examples: {len(train_loader.dataset)}')
 	print(f'Number of validation examples: {len(val_loader.dataset)}')
 
