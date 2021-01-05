6b05140166afb7fced7e998deec03ce3061fb464
Rakesh M S <msrakesh5991@gmail.com>
2021-01-05 15:04:46 +0530

    print statement

--- datasets/vehicle_attributes.py
+++ datasets/vehicle_attributes.py
@@ -19,7 +19,9 @@ def class_mapping():
 			  'bmw', 'volkswagen', 'chevrolet', 'ford', 'fiat', 'nissan', 'mercedes', 'hundai',
 			  'datson', 'force', 'datsun', 'jaguar', 'hatchback', 'suv', 'van']
 	for i, label in enumerate(labels):
+		print(labels)
 		class_to_idx[label] = i
+		print(class_to_idx)
 	return class_to_idx
 
 
